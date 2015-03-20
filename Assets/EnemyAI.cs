d3b9f941cea445f4f68b518eb19b8521516abdb1
Antonio <agarcia@ninjacodestudio.com>
2015-03-20 17:49:11 +0100

    Primera escena

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+    public PlayerController player;
+	void Start () {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        }
+	}
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce((player.transform.position-transform.position).normalized*1000);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+    void FixedUpdate()
+    {
+        Debug.Log("Fixed Update");
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.AddForce(-Vector3.right*1000);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.AddForce(Vector3.right*1000);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody.AddForce(Vector3.forward*1000);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody.AddForce(-Vector3.forward*1000);
+        }
+	}
+}
