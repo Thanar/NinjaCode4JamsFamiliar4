8393aa84f675deaa297949de50c45c87fc12e844
KyTn <angel.kytn@gmail.com>
2015-03-21 15:58:31 +0100

    lolipops

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PlayerController : Character {
 
+    public Animator animator;
+    public IntroGameController introGameController;
+
     public UI ui;
     public float MaxFocus;
     public float focus;
@@ -27,7 +30,8 @@ public class PlayerController : Character {
 
     Vector3 finalVelocity = new Vector3();
 
-
+    public bool ICanMove = false;
+    public bool Chosing = true;
 
     public GameObject specialAttackEffectPrefab;
 
@@ -35,86 +39,112 @@ public class PlayerController : Character {
 
 	// Use this for initialization
 	void Start () {
+        ICanMove = false;
+
+
 	    //CUANDO SE PONGAN LAS ARMAS QUITAR ESTA LINEA
         ui.ToggleAmmo(false);
 	}
 
     void Update()
     {
-        RaycastHit hit;
-        rayVector = Input.mousePosition;
-        rayVector.z = 10000;
-        Ray ray = Camera.main.ScreenPointToRay(rayVector);
-        if (Physics.Raycast(ray, out hit,1000f, LayerMask.GetMask("Floor")))
+
+        if (ICanMove)
         {
-            newRotation.y = Quaternion.LookRotation(hit.point - transform.position).eulerAngles.y;
-            transform.eulerAngles = newRotation;
-        }
 
-        UpdateStatus();
-
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            if (onFocus)
+            RaycastHit hit;
+            rayVector = Input.mousePosition;
+            rayVector.z = 10000;
+            Ray ray = Camera.main.ScreenPointToRay(rayVector);
+            if (Physics.Raycast(ray, out hit, 1000f, LayerMask.GetMask("Floor")))
             {
-                DeactivateFocus();
+                newRotation.y = Quaternion.LookRotation(hit.point - transform.position).eulerAngles.y;
+                transform.eulerAngles = newRotation;
             }
-            else
-            {
-                ActivateFocus();
-            }
-        }
 
-        pushForce = Vector3.Lerp(pushForce, Vector3.zero, 0.05f);
+            UpdateStatus();
 
-        if (Input.GetMouseButton(0))
-        {
-            if (hasWeapon)
+            if (Input.GetKeyDown(KeyCode.F))
             {
-                weapon.Attack();
-            }
-            else
-            {
-                if (fistsTimeReady < Time.time)
+                if (onFocus)
                 {
-                    foreach (Collider c in Physics.OverlapSphere(fistsPosition.position, 0.5f))
+                    DeactivateFocus();
+                }
+                else
+                {
+                    ActivateFocus();
+                }
+            }
+
+            pushForce = Vector3.Lerp(pushForce, Vector3.zero, 0.05f);
+
+            if (Input.GetMouseButton(0))
+            {
+                if (hasWeapon)
+                {
+                    weapon.Attack();
+                }
+                else
+                {
+                    if (fistsTimeReady < Time.time)
+                    {
+                        foreach (Collider c in Physics.OverlapSphere(fistsPosition.position, 0.5f))
+                        {
+                            auxCharacter = c.GetComponent<EnemyAI>();
+                            if (auxCharacter)
+                            {
+                                Instantiate(fistAttackEffectPrefab, fistsPosition.position, fistsPosition.rotation);
+                                fistsTimeReady = Time.time + fistsCooldown;
+                                auxCharacter.Damage(fistsDamage, fistsArmorPenetration);
+                                //auxCharacter.rigidbody.AddForce((transform.forward.normalized) * 8, ForceMode.VelocityChange);
+                                auxCharacter.Push(transform.forward.normalized * 10);
+
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (focus > 0.2f)
+                {
+                    focus -= 0.2f;
+                    ui.SetFocus();
+                    Instantiate(specialAttackEffectPrefab, transform.position, Quaternion.identity);
+                    Vector3 pushDirection;
+                    foreach (Collider c in Physics.OverlapSphere(transform.position, 5f))
                     {
                         auxCharacter = c.GetComponent<EnemyAI>();
                         if (auxCharacter)
                         {
-                            Instantiate(fistAttackEffectPrefab, fistsPosition.position, fistsPosition.rotation);
-                            fistsTimeReady = Time.time + fistsCooldown;
-                            auxCharacter.Damage(fistsDamage, fistsArmorPenetration);
+                            auxCharacter.Damage(fistsDamage * 5, fistsArmorPenetration * 2);
                             //auxCharacter.rigidbody.AddForce((transform.forward.normalized) * 8, ForceMode.VelocityChange);
-                            auxCharacter.Push(transform.forward.normalized * 10);
-                            
+                            pushDirection = (auxCharacter.transform.position - transform.position);
+                            pushDirection.y = 0;
+                            pushDirection = pushDirection.normalized * 30;
+                            auxCharacter.Push(pushDirection);
                         }
                     }
                 }
             }
         }
-
-        if (Input.GetMouseButtonDown(1))
+        else if(Chosing)
         {
-            if (focus > 0.2f)
+            if (Input.GetKey(KeyCode.A))
             {
-                focus -= 0.2f;
-                ui.SetFocus();
-                    Instantiate(specialAttackEffectPrefab, transform.position, Quaternion.identity);    
-                Vector3 pushDirection;
-                foreach (Collider c in Physics.OverlapSphere(transform.position, 5f))
+                animator.SetTrigger("Roja");
+
+                if (Input.GetKey(KeyCode.Space))
                 {
-                    auxCharacter = c.GetComponent<EnemyAI>();
-                    if (auxCharacter)
-                    {
-                        auxCharacter.Damage(fistsDamage * 5, fistsArmorPenetration * 2);
-                        //auxCharacter.rigidbody.AddForce((transform.forward.normalized) * 8, ForceMode.VelocityChange);
-                        pushDirection = (auxCharacter.transform.position - transform.position);
-                        pushDirection.y = 0;
-                        pushDirection = pushDirection.normalized * 30;
-                        auxCharacter.Push(pushDirection);
-                    }
+                    introGameController.ChooseYet = true;
                 }
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                animator.SetTrigger("Azul");
             }
         }
     }
@@ -122,54 +152,57 @@ public class PlayerController : Character {
 
     void FixedUpdate()
     {
-        finalVelocity.Set(0,0,0);
-        if (Input.GetKey(KeyCode.A))
+        if (ICanMove)
         {
-            //if (Vector3.Project(rigidbody.velocity, -Vector3.right).magnitude < maxSpeed)
-            //{
-            //    rigidbody.AddForce(-Vector3.right.normalized * charachterImpulse * movementForceScale);
-            //}
 
-            //rigidbody.velocity = -Vector3.right.normalized * maxSpeed + pushForce;
-            finalVelocity.x -=  maxSpeed;
+
+            finalVelocity.Set(0, 0, 0);
+            if (Input.GetKey(KeyCode.A))
+            {
+                //if (Vector3.Project(rigidbody.velocity, -Vector3.right).magnitude < maxSpeed)
+                //{
+                //    rigidbody.AddForce(-Vector3.right.normalized * charachterImpulse * movementForceScale);
+                //}
+
+                //rigidbody.velocity = -Vector3.right.normalized * maxSpeed + pushForce;
+                finalVelocity.x -= maxSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                //if (Vector3.Project(rigidbody.velocity, Vector3.right).magnitude < maxSpeed)
+                //{
+                //    rigidbody.AddForce(Vector3.right.normalized * charachterImpulse * movementForceScale);
+                //}
+                //rigidbody.velocity = Vector3.right.normalized * maxSpeed + pushForce;
+                finalVelocity.x += maxSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                //if (Vector3.Project(rigidbody.velocity, Vector3.forward).magnitude < maxSpeed)
+                //{
+                //    rigidbody.AddForce(Vector3.forward.normalized * charachterImpulse * movementForceScale);
+                //}
+                //rigidbody.velocity = Vector3.forward.normalized * maxSpeed + pushForce;
+                finalVelocity.z += maxSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                //if (Vector3.Project(rigidbody.velocity, -Vector3.forward).magnitude < maxSpeed)
+                //{
+                //    rigidbody.AddForce(-Vector3.forward.normalized * charachterImpulse * movementForceScale);
+                //}
+                //rigidbody.velocity = -Vector3.forward.normalized * maxSpeed + pushForce;
+                finalVelocity.z -= maxSpeed;
+            }
+
+            finalVelocity = finalVelocity.normalized * maxSpeed * movementForceScale;
+
+            rigidbody.velocity = finalVelocity + pushForce;
         }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            //if (Vector3.Project(rigidbody.velocity, Vector3.right).magnitude < maxSpeed)
-            //{
-            //    rigidbody.AddForce(Vector3.right.normalized * charachterImpulse * movementForceScale);
-            //}
-            //rigidbody.velocity = Vector3.right.normalized * maxSpeed + pushForce;
-            finalVelocity.x += maxSpeed;
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            //if (Vector3.Project(rigidbody.velocity, Vector3.forward).magnitude < maxSpeed)
-            //{
-            //    rigidbody.AddForce(Vector3.forward.normalized * charachterImpulse * movementForceScale);
-            //}
-            //rigidbody.velocity = Vector3.forward.normalized * maxSpeed + pushForce;
-            finalVelocity.z += maxSpeed;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            //if (Vector3.Project(rigidbody.velocity, -Vector3.forward).magnitude < maxSpeed)
-            //{
-            //    rigidbody.AddForce(-Vector3.forward.normalized * charachterImpulse * movementForceScale);
-            //}
-            //rigidbody.velocity = -Vector3.forward.normalized * maxSpeed + pushForce;
-            finalVelocity.z -= maxSpeed;
-        }
-
-        finalVelocity = finalVelocity.normalized * maxSpeed*movementForceScale;
-
-        rigidbody.velocity = finalVelocity+pushForce;
-
-        
-	}
+    }
 
     public void ActivateFocus()
     {
