4e97be7535984b194909efa7d8d5a97072412cb4
sephi6 <vdominguezaragon@gmail.com>
2015-03-21 14:08:52 +0100

    curaga

---
+++ Assets/Scripts/Heal.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Heal : MonoBehaviour {
+
+    public float heal = 25f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triger: " + other.tag);
+        if (other.tag == "Player")
+        {
+
+            PlayerController p = other.GetComponent<PlayerController>();
+            p.health += heal;
+            Destroy(this.gameObject);
+
+            
+        }
+    }
+}
