1bb99a47f20d099fff6066c01113d2a2eff9cd13
sephi6 <vdominguezaragon@gmail.com>
2015-03-21 15:32:34 +0100

    redpill y heal

---
+++ Assets/Scripts/RedPill.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedPill : MonoBehaviour {
+
+    public float focus = 25f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triger: " + other.tag);
+        if (other.tag == "Player")
+        {
+
+            PlayerController p = other.GetComponent<PlayerController>();
+            p.focus += focus;
+            Destroy(this.gameObject);
+
+
+        }
+    }
+}
