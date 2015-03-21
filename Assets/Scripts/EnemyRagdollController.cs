cfce05ad075ac961415918cc1b2d02b2988ba36a
Antonio <agarcia@ninjacodestudio.com>
2015-03-21 20:00:04 +0100

    ragdoll

---
+++ Assets/Scripts/EnemyRagdollController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyRagdollController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 7);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
