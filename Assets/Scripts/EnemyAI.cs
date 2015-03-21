d1340ee08af8ffdf072f6d4937ffd57d75fa086d
Antonio <agarcia@ninjacodestudio.com>
2015-03-21 10:30:55 +0100

    esquivar

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -14,7 +14,7 @@ public class EnemyAI : Character {
     Vector3 newRotation = new Vector3(0, 0, 0);
     Vector3 player2DDirection = new Vector3(0,0,0);
 
-
+    Vector3 auxVector = new Vector3(0, 0, 0);
 
 
     public void Push(Vector3 direction)
@@ -98,15 +98,72 @@ public class EnemyAI : Character {
 
     void FixedUpdate()
     {
-        player2DDirection = (player.transform.position - transform.position).normalized;
+        player2DDirection = (player.transform.position - transform.position);
         player2DDirection.y = 0;
+        player2DDirection.Normalize();
         //if (Vector3.Project(rigidbody.velocity, player2DDirection).magnitude < maxSpeed)
         //{
         //    rigidbody.AddForce(player2DDirection * charachterImpulse);
         //}
-        rigidbody.velocity = (player2DDirection * maxSpeed + pushForce)+(Vector3.up * rigidbody.velocity.y);
+        float playerDistance = (player.transform.position - transform.position).magnitude;
+        if (hasWeapon)
+        {
+            if ( playerDistance > 10)
+            {
+                MoveTowardsPlayer();
+            }
+            else if (playerDistance > 6)
+            {
+                MoveAlongPlayer();
+            }
+            else
+            {
+                MoveAwayFromPlayerDodging();
+            }
+        }
+        else
+        {
+            if (health > 10)
+            {
+                MoveTowardsPlayer();
+            }
+            else
+            {
+                MoveAwayFromPlayer();
+            }
+        }
     }
 
+    public void MoveAwayFromPlayer()
+    {
+        rigidbody.velocity = -(player2DDirection * maxSpeed + pushForce) + (Vector3.up * rigidbody.velocity.y);
+    }
+
+    public void MoveAwayFromPlayerDodging()
+    {
+        auxVector.x = -player2DDirection.z;
+        auxVector.z = player2DDirection.x;
+
+        player2DDirection += auxVector;
+
+        player2DDirection.Normalize();
+
+        rigidbody.velocity = (player2DDirection * maxSpeed + pushForce) + (Vector3.up * rigidbody.velocity.y);
+    }
+
+    public void MoveTowardsPlayer()
+    {
+        rigidbody.velocity = (player2DDirection * maxSpeed + pushForce) + (Vector3.up * rigidbody.velocity.y);
+    }
+
+    public void MoveAlongPlayer()
+    {
+        float aux = player2DDirection.x;
+        player2DDirection.x = player2DDirection.z;
+        player2DDirection.z = -aux;
+        player2DDirection.Normalize();
+        rigidbody.velocity = (player2DDirection * maxSpeed + pushForce) + (Vector3.up * rigidbody.velocity.y);
+    }
 
 
     public override void Die()
