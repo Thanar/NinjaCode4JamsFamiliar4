0007f52652072bd5b4febc308d2d1fed9d2bcab5
Antonio <agarcia@ninjacodestudio.com>
2015-03-21 01:10:39 +0100

    UZI

--- Assets/Scripts/FireWeapon.cs
+++ Assets/Scripts/FireWeapon.cs
@@ -6,7 +6,10 @@ public class FireWeapon : Weapon {
     public float nextTimeReady = 0;
     public float cooldown = 1;
 
+    public float reloadTime = 3;
+
     public int bulletsTotal = 30;
+    public int currentBullets = 12;
     public int bulletsPerCharge = 12;
 
     public Transform bulletSpawnPoint;
@@ -20,13 +23,36 @@ public class FireWeapon : Weapon {
         {
             return;
         }
+        if (currentBullets > 0)
+        {
+            Bullet myBullet = ((GameObject)Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation)).GetComponent<Bullet>();
 
-        Bullet myBullet = ((GameObject)Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation)).GetComponent<Bullet>();
+            myBullet.damage = this.damage;
+            myBullet.armorPenetration = this.armorPenetration;
+            myBullet.rigidbody.velocity = transform.forward.normalized * 100;
 
-        myBullet.damage = this.damage;
-        myBullet.armorPenetration = this.armorPenetration;
-        myBullet.rigidbody.velocity = transform.forward.normalized * 100;
+            nextTimeReady = Time.time + cooldown;
 
-        nextTimeReady = Time.time + cooldown;
+            currentBullets--;
+        }
+        else
+        {
+            if (bulletsTotal > 0)
+            {
+                bulletsTotal -= bulletsPerCharge;
+                if (bulletsTotal < 0)
+                {
+                    bulletsTotal = 0;
+                }
+
+                currentBullets = bulletsPerCharge;
+                if (bulletsTotal < currentBullets)
+                {
+                    currentBullets = bulletsTotal;
+                }
+                nextTimeReady = Time.time + reloadTime;
+            }
+        }
+
     }
 }
