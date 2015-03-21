ec811072743662cde0b6f1ec0eedcde9086d7425
Antonio <agarcia@ninjacodestudio.com>
2015-03-21 16:20:09 +0100

    ShotGun

---
+++ Assets/Scripts/ShotGun.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotGun : FireWeapon {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void Attack()
+    {
+        base.Attack();
+        if (nextTimeReady > Time.time)
+        {
+            return;
+        }
+        if (currentBullets > 0)
+        {
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+                Bullet myBullet = ((GameObject)Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation)).GetComponent<Bullet>();
+
+                myBullet.damage = this.damage;
+                myBullet.armorPenetration = this.armorPenetration;
+                myBullet.rigidbody.velocity = (bulletSpawnPoint.forward + bulletSpawnPoint.right * Random.Range(-0.02f, 0.02f) + bulletSpawnPoint.up * Random.Range(-0.02f, 0.02f)).normalized * 100;
+            }
+
+                nextTimeReady = Time.time + cooldown;
+
+            currentBullets--;
+        }
+        else
+        {
+            if (bulletsTotal > 0)
+            {
+                bulletsTotal -= bulletsPerCharge;
+                if (bulletsTotal < 0)
+                {
+                    bulletsTotal = 0;
+                }
+
+                currentBullets = bulletsPerCharge;
+                if (bulletsTotal < currentBullets)
+                {
+                    currentBullets = bulletsTotal;
+                }
+                nextTimeReady = Time.time + reloadTime;
+            }
+        }
+
+    }
+}
