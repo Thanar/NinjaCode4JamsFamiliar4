b6f8b1095e4a125046074fc70940c0076f1e43de
sephi6 <vdominguezaragon@gmail.com>
2015-03-22 13:35:32 +0100

    sonido enemigos funcional

--- Assets/Scripts/EnemyRagdollController.cs
+++ Assets/Scripts/EnemyRagdollController.cs
@@ -5,9 +5,14 @@ public class EnemyRagdollController : MonoBehaviour {
 
     public Rigidbody mainRigidbody;
 
+    public AudioSource[] sonidos;
+
 	// Use this for initialization
 	void Start () {
         Destroy(gameObject, 7);
+        int i = Random.Range(0, sonidos.Length);
+        AudioSource j = sonidos[i];
+        j.Play();
 	}
 	
 	// Update is called once per frame
